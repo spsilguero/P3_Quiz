23c1d5c919facf3afa6e681fd560d06f5b7f7ecc
spsilguero <s.perez-silguero@alumnos.upm.es>
2018-03-11 23:36:29 +0100

    v1.4

--- cmds.js
+++ cmds.js
@@ -205,7 +205,8 @@ exports.testCmd = (rl, id) => {
     });
   })
   .catch(error => {
-    errorlog(error.message);
+    console.log(error.message);
+    //errorlog(error.message);
   })
   .then(() => {
     rl.prompt();
